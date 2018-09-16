b291f4f6044ce52fc5bd26f39bc2561a1ab10214
memocusss <mehmetcelem@gmail.com>
2018-09-16 23:30:08 +0300

    web mode removed unnecessary lines

--- init.el
+++ init.el
@@ -99,14 +99,6 @@
         ("css"    . "\\.css\\'"))
       )
 
-(add-to-list 'auto-mode-alist '("\\.api\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("/some/react/path/.*\\.js[x]?\\'" . web-mode))
-
-(setq web-mode-content-types-alist
-  '(("json" . "/some/path/.*\\.api\\'")
-    ("xml"  . "/other/path/.*\\.api\\'")
-    ("jsx"  . "/some/react/path/.*\\.js[x]?\\'")))
-
 ;; HELM CONFIGURATION
 ;; --------------------------------------
 (require 'helm-config)
