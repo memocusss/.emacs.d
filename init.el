d04464daaedc3517d741870cf5ccd07795f28cf4
memocusss <mehmetcelem@gmail.comm>
2018-12-11 00:11:36 +0300

    updates

--- init.el
+++ init.el
@@ -54,7 +54,6 @@
 (powerline-default-theme)
 (ido-mode 1)
 (setq ido-enable-flex-matching t)
-(setq ido-everywhere t)
 (global-set-key (kbd "M-o") 'other-window)
 (require 'all-the-icons)
 (require 'yasnippet)
@@ -151,3 +150,17 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (request-deferred request pyvenv magit-popup ivy highlight-indentation helm-core git-commit ghub find-file-in-project company web-mode use-package snippet restart-emacs py-autopep8 projectile powerline neotree material-theme magit helm flycheck emmet-mode elpy ein company-web better-defaults beacon atom-one-dark-theme all-the-icons))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
