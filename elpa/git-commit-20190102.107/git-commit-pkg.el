9e1bcfcd0a79eac46767e415b0b4931602e041a5
memocusss <mehmetcelem@gmail.com>
2019-07-20 02:01:18 +0300

    standart updates

--- elpa/git-commit-20190102.107/git-commit-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "git-commit" "20190102.107" "Edit Git commit messages" '((emacs "25.1") (dash "20180910") (with-editor "20181103")) :commit "b4aec016b5577afa8d889f258b499814d1bb1d94" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
