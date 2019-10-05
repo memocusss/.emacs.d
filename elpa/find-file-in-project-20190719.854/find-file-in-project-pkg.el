ca1ae2b79c0fa6b83d4f79ffa6ec62fa56a73334
memocusss <mehmetcelem@gmail.com>
2019-10-05 17:20:38 +0300

    standart updates

--- elpa/find-file-in-project-20190719.854/find-file-in-project-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "find-file-in-project" "20190719.854" "Find file/directory and review Diff/Patch/Commit efficiently everywhere" '((ivy "0.10.0") (emacs "24.4")) :commit "566ad7c1790d2f0f156d46b3e3fff85722e84b44" :keywords '("project" "convenience") :authors '(("Phil Hagelberg, Doug Alcorn, and Will Farrington")) :maintainer '("Chen Bin" . "chenbin.sh@gmail.com") :url "https://github.com/technomancy/find-file-in-project")
--- elpa/w32-browser-20170101.1954/w32-browser-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "w32-browser" "20170101.1954" "Run Windows application associated with a file." 'nil :commit "e5c60eafd8f8d3546a0fa295ad5af2414d36b4e6" :keywords '("mouse" "dired" "w32" "explorer") :authors '(("Emacs Wiki, Drew Adams")) :maintainer '(nil . "Drew Adams (concat \"drew.adams\" \"@\" \"oracle\" \".com\")") :url "http://www.emacswiki.org/w32-browser.el")
